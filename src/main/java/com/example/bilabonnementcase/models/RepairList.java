4372fd8e2654372e9a416557f874e5099220ac0f
gulernurdonmez <gule0153@stud.kea.dk>
2022-05-09 19:55:43 +0200

    minor changes

--- src/main/java/com/example/bilabonnementcase/models/RepairList.java
+++ src/main/java/com/example/bilabonnementcase/models/RepairList.java
@@ -4,14 +4,19 @@ public class RepairList {
 
     private int repairListId;
     private String damageTitle;
+    private String missingCarPieceTitle;
     private String damageDescription;
+    private String missingCarPieceDescription;
     private double damagePrice;
     private Car carNumber;
 
-    public RepairList(int repairListId, String damageTitle, String damageDescription, double damagePrice, Car carNumber) {
+    public RepairList(int repairListId, String damageTitle, String missingCarPieceTitle, String damageDescription,
+                      String missingCarPieceDescription, double damagePrice, Car carNumber) {
         this.repairListId = repairListId;
         this.damageTitle = damageTitle;
+        this.missingCarPieceTitle = missingCarPieceTitle;
         this.damageDescription = damageDescription;
+        this.missingCarPieceDescription = missingCarPieceDescription;
         this.damagePrice = damagePrice;
         this.carNumber = carNumber;
     }
@@ -24,8 +29,16 @@ public class RepairList {
         return damageTitle;
     }
 
+    public String getMissingCarPieceTitle() {
+        return missingCarPieceTitle;
+    }
+
     public String getDamageDescription() {
         return damageDescription;
+    }
+
+    public String getMissingCarPieceDescription() {
+        return missingCarPieceDescription;
     }
 
     public double getDamagePrice() {
@@ -41,7 +54,9 @@ public class RepairList {
         return "RepairList{" +
                 "repairListId=" + repairListId +
                 ", damageTitle='" + damageTitle + '\'' +
+                ", missingCarPieceTitle='" + missingCarPieceTitle + '\'' +
                 ", damageDescription='" + damageDescription + '\'' +
+                ", missingCarPieceDescription='" + missingCarPieceDescription + '\'' +
                 ", damagePrice=" + damagePrice +
                 ", carNumber=" + carNumber +
                 '}';
