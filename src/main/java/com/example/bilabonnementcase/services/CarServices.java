ccea3c1b3dc9197587532b7ed22c629ca38fdc02
majamatboue <maja962h@stud.kea.dk>
2022-05-10 12:34:36 +0200

    edited Car

--- src/main/java/com/example/bilabonnementcase/services/CarServices.java
+++ src/main/java/com/example/bilabonnementcase/services/CarServices.java
@@ -1,6 +1,11 @@
 package com.example.bilabonnementcase.services;
 
+import com.example.bilabonnementcase.models.Car;
+import com.example.bilabonnementcase.repositories.CarRepository;
+
 public class CarServices {
+    CarRepository carRepository = new CarRepository();
+
 
 
 }
