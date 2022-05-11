b42f94c66db52d61f4423f8fa7457a137c5b3ce6
chris <chri402s@stud.kea.dk>
2022-05-11 10:51:32 +0200

    index.html links works

---
+++ src/main/java/com/example/bilabonnementcase/controllers/StatistController.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnementcase.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StatistController {
+
+    @GetMapping("/viewStatistics")
+    public String createRepairList(){
+        return "viewStatistics";
+    }
+}
