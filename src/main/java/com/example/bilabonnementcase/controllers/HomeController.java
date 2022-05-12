481744687612d4a450df1f1cf99fb68b13c64c10
majamatboue <maja962h@stud.kea.dk>
2022-05-12 09:21:26 +0200

    added get mapping to errorPage.html

--- src/main/java/com/example/bilabonnementcase/controllers/HomeController.java
+++ src/main/java/com/example/bilabonnementcase/controllers/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/error-page")
+    public String errorPage(){
+        return "errorPage";
+    }
 }
