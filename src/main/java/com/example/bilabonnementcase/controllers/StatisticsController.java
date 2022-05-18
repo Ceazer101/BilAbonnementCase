020c207c531bf6a50a290dda10659a358bb34081
Kennlf <kenn741q@stud.kea.dk>
2022-05-18 10:01:23 +0200

    added html statistics

--- src/main/java/com/example/bilabonnementcase/controllers/StatisticsController.java
+++ src/main/java/com/example/bilabonnementcase/controllers/StatisticsController.java
@@ -1,13 +1,21 @@
 package com.example.bilabonnementcase.controllers;
 
+import com.example.bilabonnementcase.services.StatisticsServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StatisticsController {
 
+    StatisticsServices service = new StatisticsServices();
+
     @GetMapping("/viewStatistics")
-    public String viewStatistics(){
+    public String viewStatistics(Model model){
+
+        model.addAttribute("rentedcars", service.rentedCars() );
+        model.addAttribute("valueOfRentedCars", service.valueOfRentedCars());
+
         return "viewPages/viewStatistics";
     }
 }
--- src/main/java/com/example/bilabonnementcase/services/StatisticsServices.java
+++ src/main/java/com/example/bilabonnementcase/services/StatisticsServices.java
@@ -1,6 +1,7 @@
 package com.example.bilabonnementcase.services;
 
 import com.example.bilabonnementcase.models.Car;
+import com.example.bilabonnementcase.repositories.StatisticsRepository;
 import com.example.bilabonnementcase.utility.DatabaseConnectionManager;
 import com.mysql.cj.protocol.Resultset;
 import com.mysql.cj.xdevapi.PreparableStatement;
@@ -9,6 +10,16 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class StatisticsServices {
+
+    StatisticsRepository sr = new StatisticsRepository();
+
+    public int rentedCars(){
+        return sr.carsRented();
+    }
+
+    public int valueOfRentedCars(){
+        return sr.rentedCarsValue();
+    }
 
 
 }
